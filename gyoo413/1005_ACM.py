88e726b288fcc217bd631f3f57ded9dee9cb6007
이연규 <gyoo413@naver.com>
2024-10-16 18:01:08 +0900

    gyoo413

--- gyoo413/1005_ACM.py
+++ gyoo413/1005_ACM.py
@@ -1,44 +1,26 @@
-import heapq
+from collections import deque
 
 t = int(input())
 for _ in range(t):
     n, k = map(int, input().split())
     time = [0] + list(map(int, input().split()))
     graph = [[] for _ in range(n + 1)]
-    order = [[] for _ in range(n + 1)]
     for _ in range(k):
         v1, v2 = map(int, input().split())
-        graph[v1].append(v2)
-        order[v2].append(v1)
+        graph[v2].append(v1)
     target = int(input())
-    start = []
-    for i in range(1, len(order)):
-        if not order[i]:
-            start.append((0, i))
 
+    queue = deque()
+    visited = [0] * (n + 1)
+    visited[target] = time[target]
+    queue.append(target)
+
+    while queue:
+        now = queue.popleft()
+        for next_node in graph[now]:
+            if visited[next_node] < visited[now] + time[next_node]:
+                visited[next_node] = visited[now] + time[next_node]
+                queue.append(next_node)
+
+    print(max(visited))    
     
-    def bfs():
-        global n, target
-
-        heap = heapq.heapify(start)
-        visited = [0] * (n + 1)
-        parent = [[0] for _ in range(n + 1)]
-        for cost, node in start:
-            visited[node] = 1
-
-        duration = 0
-        
-        while heap:
-            now = heapq.heappop(heap)
-            for next_node in graph[now]:
-                if visited[next_node] < len(order[next_node]) - 1:
-                    visited[next_node] += 1
-                    parent[next_node].append(time[now])
-                elif visited[next_node] == len(order[next_node]) - 1:
-                    visited[next_node] += 1
-                    parent[next_node].append(time[now])
-                    heapq.heappush(heap, (max(parent[next_node]), next_node))
-                    
-                    
-    print(bfs())
-
